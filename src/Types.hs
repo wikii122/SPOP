ad8f4de6202c7cb6b2e7960b625d73294d50cbc6
Wiktor <wikii122@gmail.com>
2016-01-04 20:48:41 +0100

    Added read and show typeclasses for Creek

--- src/Types.hs
+++ src/Types.hs
@@ -2,4 +2,8 @@ type Size = (Int, Int)
 type Loction = (Int, Int)
 type Condition = (Location, Int)
 
-data Creek = Creek Size [Conditions]
+data Creek = Creek Size [Conditions] 
+  deriving (
+    Show,
+    Read
+    )
