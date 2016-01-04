a5c97a425bf40a30f36c79e8038e6b4a834f6749
Wiktor <wikii122@gmail.com>
2016-01-04 21:44:20 +0100

    Added basic IO loop

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,20 @@
-main = putStrLn "Hello World"
+import System.Environment
+
+import Creek
+
+main :: IO ()
+main = do
+  args <- getArgs
+  parse_arguments args
+
+parse_arguments :: [String] -> IO ()
+parse_arguments ["-h"] = error "Not implemented"
+parse_arguments [path] = do
+  obj       <- readFile path
+  result    <- return . solve $ read_creek obj
+  putStrLn $ show result
+    where
+      read_creek creek = read creek :: Creek
+parse_arguments _         = error "Unknown input, try '-h'"
+
+solve x = error "Not implemented"
