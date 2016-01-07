ab94a5f354cc41f4e6ee96d0c4b8bc0b8a767de4
Wiktor <wikii122@gmail.com>
2016-01-07 12:38:45 +0100

    Added constructor and get operators

--- src/Board.hs
+++ src/Board.hs
@@ -1,9 +1,38 @@
-module Board 
+module Board
   ( Field
   , Board
-  ) where 
+  ) where
+
+import Creek (Location, Size, Creek)
 
 data Field = White | Black | Unknown
   deriving (Show, Eq)
 
+type Quad = ((Field, Field), (Field, Field))
+
 type Board = [[Field]]
+
+-- Constructor for empty board
+empty :: Size -> Board
+empty (x, y) = replicate y $ replicate x Unknown
+
+-- Get value from a field
+field :: Board -> Location -> Field
+field board (x, y) = (board !! y) !! x
+
+-- Get four values from crossing
+quad :: Board -> Location -> Quad
+quad board (x, y) = ((ys1 !! x, ys1 !! (x+1)),
+                     (ys2 !! x, ys2 !! (x+1)))
+                      where
+                        ys1 = board !! y
+                        ys2 = board !! (y+1)
+
+-- Operators for field and quad operations
+-- Get field operator
+(!) :: Board -> Location -> Field
+board ! loc = field board loc
+
+-- Get quad operator
+(!#) :: Board -> Location -> Quad
+board !# loc = quad board loc
--- src/Creek.hs
+++ src/Creek.hs
@@ -1,16 +1,13 @@
-module Creek 
+module Creek
   ( Creek
   , Size
   , Location
   , Condition
-) where 
+) where
 
 type Size = (Int, Int)
 type Location = (Int, Int)
 type Condition = (Location, Int)
 
-data Creek = Creek Size [Condition] 
-  deriving (
-    Show,
-    Read
-    )
+data Creek = Creek Size [Condition]
+  deriving (Show, Read)
